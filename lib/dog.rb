35fd6facd67974a6629dbf37d286097345b3bae9
Dexter <dex429.dc@gmail.com>
2021-01-06 21:40:06 -0500

    commit tree 71a0245390af726945afdc2746e4cc3cb7dcd5a5

---
+++ lib/dog.rb
@@ -0,0 +1,28 @@
+class Dog
+    def initialize(name, breed="Mutt")
+        @name = name
+        @breed = breed
+        
+      end
+    
+      def name=(name)
+        @name = name
+      end
+
+     def breed=(breed)
+        @dog_breed = breed
+        end
+    
+   
+        
+    
+      def name
+        @name
+      end
+
+      def breed
+        @breed
+      end
+end
+
+
